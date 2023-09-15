05c3053f95ad4b4ddbc5f62729b61655a5d6f7ea
FernandaMendex <fernandamendesq@hotmail.com>
2023-09-15 02:13:18 -0300

    perf: criação de um banco de dados para alimentação das páginas

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":1,
+    "image":"",
+    "title":"",
+    "description":"",
+  },
+  {
+    "id":2,
+    "image":"",
+    "title":"",
+    "description":"",
+  }
+]
